cbae83cabee1873b0cc6f723115433cfd5a7dd60
samikhan1622 <153662652+samikhan1622@users.noreply.github.com>
2024-08-29 13:29:38 +0500

    main

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -139,7 +139,7 @@ const Work = () => {
               ))}
               <WorkSliderBtn 
                 containerStyle="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-center xl:w-max xl:justify-none"
-                btnStyle="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-between items-center transition-all"
+                btnStyle="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
               />
             </Swiper>
           </div>
